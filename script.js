431d9a7e976279610e49655a2c91ea80cd587f2e
Ibrahima SOURABIE <ge65yub@mytum.de>
2023-11-09 23:14:09 +0100

    Html and Css actualised

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-"use strict";
+'use strict';
+
+let question = [
+  {
+    question: 'Wer hat HTML erfunden?',
+    answer_1: 'Ronnie Williams',
+    answer_2: 'Lady Gaga',
+    answer_3: 'Tim Berners-Lee',
+    answer_4: 'Justin Bieber',
+    right_answer: 3,
+  },
+];
